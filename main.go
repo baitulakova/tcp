0d5a42fa1af3b401d7719ddacc0e436236de5ddc
baitulakova <baitulakova13@gmail.com>
2018-11-01 08:17:53 +0600

    added new functions Addr and SendString

--- main.go
+++ main.go
@@ -45,22 +45,33 @@ type Client struct{
 	conn net.Conn
 }
 
+func (c *Client) Addr()string{
+	return c.conn.RemoteAddr().String()
+}
+
 func (c *Client) Close(){
 	c.conn.Close()
+	log.Printf("Connection with %v closed",c.Addr())
+}
+
+func (c *Client) SendString(message string){
+	c.conn.Write([]byte(message))
 }
 
 func (c *Client) handleConnection(){
-	log.Println("Listening for connection on: ",c.conn.RemoteAddr().String())
+	log.Println("Listening for connection on: ",c.Addr())
 	defer c.Close()
+	c.SendString("Hello " + c.Addr()+"\n")
 	buf := make([]byte, 1024)
 	_, err := c.conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading:", err.Error())
 	}
+	c.SendString("You entered: ")
 	c.conn.Write(buf)
 }
 
 func main(){
-	s:=NewServer("","8080")
+	s:=NewServer("127.0.1.1","8088")
 	s.Listen()
 }
