99dd1d3cdabd909dd0798c7fa700dbfc00efbc3c
baitulakova <baitulakova13@gmail.com>
2018-11-01 07:52:50 +0600

    added structs server and client

--- main.go
+++ main.go
@@ -2,34 +2,65 @@ package main
 
 import (
 	"net"
-	"os"
-	"fmt"
 	"log"
+	"fmt"
+	"os"
 )
 
-func handleConnection(conn net.Conn){
-	defer conn.Close()
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading:", err.Error())
-	}
-	conn.Write(buf)
+type Server struct {
+	address string
+	port string
 }
 
-func main(){
-	listener,err:=net.Listen("tcp", ":8080")
+func NewServer(addr,p string) *Server{
+	log.Printf("Creating new server with address %v and port %v",addr,p)
+	server:=&Server{
+		address:addr,
+		port:p,
+	}
+	return server
+}
+
+func (s *Server) Listen(){
+	log.Println("Listening to server: ",s.address+":"+s.port)
+	listener,err:=net.Listen("tcp", s.address+":"+s.port)
 	if err!=nil{
 		log.Fatal("Error listening: ",err)
 		os.Exit(1)
 	}
-
 	for{
 		conn,err:=listener.Accept()
 		if err!=nil{
+			log.Println("Error accepting connections: ",err)
 			panic(err)
 		}
-		conn.Write([]byte("TCP Socket\n"))
-		go handleConnection(conn)
+		client:=&Client{
+			conn:conn,
+		}
+		go client.handleConnection()
 	}
+}
+
+type Client struct{
+	conn net.Conn
+}
+
+func (c *Client) Close(){
+	c.conn.Close()
+}
+
+func (c *Client) handleConnection(){
+	log.Println("Listening for connection on: ",c.conn.RemoteAddr().String())
+	defer c.Close()
+	buf := make([]byte, 1024)
+	_, err := c.conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	c.conn.Write(buf)
+}
+
+func main(){
+	s:=NewServer("","8080")
+	s.Listen()
 }
