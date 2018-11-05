c72b6079113a632f030d75d385acfab3539229f6
baitulakova <baitulakova13@gmail.com>
2018-11-05 09:57:03 +0600

    implemented server side of file transfer

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"log"
 	"os"
 	"fmt"
+	"bufio"
+	"io"
+	"flag"
+)
+
+var(
+	command = flag.String("c","","choose command")
 )
 
 type Server struct {
@@ -22,6 +29,8 @@ func NewServer(addr,p string) *Server{
 }
 
 func (s *Server) Listen(){
+	flag.Parse()
+	log.Println("c: ",*command)
 	log.Println("Listening to server: ",s.address+":"+s.port)
 	listener,err:=net.Listen("tcp", s.address+":"+s.port)
 	if err!=nil{
@@ -38,7 +47,7 @@ func (s *Server) Listen(){
 		client:=&Client{
 			conn:conn,
 		}
-		go client.handleConnection()
+		go client.handleConnection(*command)
 	}
 }
 
@@ -59,28 +68,59 @@ func (c *Client) SendString(message string){
 	c.conn.Write([]byte(message))
 }
 
-func (c *Client) handleConnection(){
+func (c *Client) handleConnection(command string){
 	log.Println("Listening for connection on: ", c.Addr())
 	c.SendString("Hello " + c.Addr() + "\n")
-	for {
-		input := make([]byte, 1024)
-		n, err := c.conn.Read(input)
-		if string(input)=="exit"{
-			break
-		}
-		log.Println("get from client: ",string(input))
-		if err != nil || n == 0 {
-			fmt.Println("Error reading:", err.Error())
+	if command=="file"{
+		err:=fileTransfer(c.conn)
+		if err!=nil{
+			log.Fatal("Error in fileTransfer: ", err)
 			os.Exit(1)
 		}
-		_, e := c.conn.Write(input)
-		if e != nil {
-			fmt.Println("error sending to client: ", e)
+	}else {
+		for {
+			input := make([]byte, 1024)
+			n, err := c.conn.Read(input)
+			log.Println("get from client: ", string(input))
+			if err != nil || n == 0 {
+				fmt.Println("Error reading:", err.Error())
+				os.Exit(1)
+			}
+			if string(input) == "exit" {
+				c.Close()
+				os.Exit(1)
+			}
+			_, e := c.conn.Write(input)
+			if e != nil {
+				fmt.Println("error sending to client: ", e)
+			}
+			log.Println("sending to client: ", string(input))
+			input = input[:0]
 		}
-		log.Println("sending to client: ",string(input))
-		input=input[:0]
 	}
 	c.conn.Close()
+}
+
+func fileTransfer(conn net.Conn) error {
+	log.Println("In file transfer")
+	defer conn.Close()
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Input path to file: ")
+	scanner.Scan()
+	input := scanner.Text()
+	f,errOpenFile:=os.Open(input)
+	if errOpenFile!=nil{
+		log.Fatal("File not found. No such file.")
+		os.Exit(1)
+	}
+	file, err := os.Create(f.Name())
+	if err != nil {
+		log.Println("Error in create")
+		return err
+	}
+	_, err = io.Copy(file, f)
+	log.Println("End of transfer")
+	return err
 }
 
 func main(){
